619c1cb0bb4ae61f8eeeae72474dacd8ac4e9856
masonMO25 <masonneale@gmail.com>
2023-02-16 12:24:52 -0600

    added local storage

--- Develop/script.js
+++ Develop/script.js
@@ -35,3 +35,14 @@ $(document).ready(function () {
 
     findTime();
 })
+
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
